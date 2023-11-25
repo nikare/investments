86a7debde17e1a8956fabc467fc44d875f5b29a2
Nikolay Karetskiy <nkaretskiy@novakidschool.com>
2023-11-25 20:05:59 +0200

    update data

--- src/constants.ts
+++ src/constants.ts
@@ -21,5 +21,6 @@ export const REAL_RESULTS: {
   3: { invested: 150000, capital: 472023.98 }, // 31 Июля 2023
   4: { invested: 150000, capital: 702138.33 }, // 31 Августа 2023
   5: { invested: 250000, capital: 989675.62 }, // 30 Сентября 2023
-  6: { invested: 250000, capital: 1309637.37 }, // 31 Октября 2023
+  6: { invested: 250600, capital: 1282725.85 }, // 31 Октября 2023
+  7: { invested: 249400, capital: 1627805.28 }, // 30 Ноября 2023
 };
