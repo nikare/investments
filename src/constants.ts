9800cff651bee3bf5a5c1604ac7f83a1cdef57c7
Nikolay Karetskiy <nkaretskiy@novakidschool.com>
2025-06-01 00:10:18 +0700

    update data

--- src/constants.ts
+++ src/constants.ts
@@ -74,8 +74,8 @@ export const REAL_RESULTS: {
   22: { invested: 250000, capitalOnLastDay: 6706475.13 }, // 28 Февраля 2025
   23: { invested: 250000, capitalOnLastDay: 6483764.19 }, // 31 Марта 2025
   24: { invested: 250000, capitalOnLastDay: 6653373.23 }, // 30 Апреля 2025
-  25: { invested: 250000, capitalOnLastDay: 0 }, // 31 мая 2025
-  // 26: { invested: 0, capitalOnLastDay: 0 }, // 30 июня 2025
+  25: { invested: 250000, capitalOnLastDay: 6789771.64 }, // 31 мая 2025
+  26: { invested: 0, capitalOnLastDay: 0 }, // 30 июня 2025
   // 27: { invested: 0, capitalOnLastDay: 0 }, // 31 июля 2025
   // 28: { invested: 0, capitalOnLastDay: 0 }, // 31 августа 2025
   // 29: { invested: 0, capitalOnLastDay: 0 }, // 30 сентября 2025
